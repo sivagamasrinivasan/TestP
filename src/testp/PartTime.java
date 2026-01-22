567e570537fc6a4509527019b308dfdedbfd4f55
sivagamasrinivasan <784108@pdsb.net>
2026-01-22 13:27:40 -0500

    branch added class

---
+++ src/testp/PartTime.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testp;
+
+/**
+ *
+ * @author sivag
+ */
+public class PartTime {
+    private int sid;
+
+    /**
+     * @return the sid
+     */
+    public int getSid() {
+        return sid;
+    }
+
+    /**
+     * @param sid the sid to set
+     */
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+    
+}
